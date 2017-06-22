2c97249838f4c0687a33ef9cae5a739ad1ef1219
Maerk Q <wj_hilliard@yahoo.com>
2017-06-22 17:03:27 -0400

    needs refinement of timers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,103 @@
-//  Declare choice 0 or 1. it's random. will use for true/false
- // var randomNumber = Math.floor(Math.random() *2);
-  //will either be a just a pokemon, or also a noun
-  var quiz = [
-  {
-  	"name": "Drowzee",
-  	"answer": "pokémon",
-  	"showAnswer": "A pokémon" 
-  }
+alert ("Pocket monsters share their names with many real words. Try to spot the differences in 60 seconds. Hint: Some are the same!" )
 
-  {
-  	"name": "Onyx",
-  	"answer": "poké-sound",
-  	"showAnswer": "A pokémon" 
-  }
+//timer
 
-    {
-  	"name": "Wheezing",
-  	"answer": "poké-sound",
-  	"showAnswer": "A pokémon" 
-  }
-    {
-  	"name": "Horsey",
-  	"answer": "poké-sound",
-  	"showAnswer": "A pokémon" 
-  }
+	var audio = new Audio("Pokemon_theme.mp3");
+function countDown (secs, elem) {
+	var element=document.getElementById(elem);
+	element.innerHTML = secs + " seconds!";
+	if(secs<1) {
+		clearTimeout(timer);
+		element.innerHTML = '<h4>Time is up! Check your score!</h4>';
+		audio.play();
+	}
+	secs--;
+	var timer=setTimeout('countDown('+secs+',"'+elem+'")',1000);
+}
 
-  {
-  	"name": "Muk",
-  	"answer": "pokémon",
-  	"showAnswer": "A pokémon" 
-  }
+//Questions, Choices, Answers
+function check(){
+var catchy = new Audio("Pokeball_pop.wav");	
+var correct=0;
+var incorrect=0;
+//got help for logic. need to refine
+var question1=document.quiz.question1.value;
+var question2=document.quiz.question2.value;
+var question3=document.quiz.question3.value;
+var question4=document.quiz.question4.value;
+var question5=document.quiz.question5.value;
+var question6=document.quiz.question6.value;
+var question7=document.quiz.question7.value;
+var question8=document.quiz.question8.value;
+var question9=document.quiz.question9.value;
+var question10=document.quiz.question10.value;
 
-  ]
 
-  $(document).ready(function() {
-  	var select = quiz[Math.floor(Math.random() *6)];
-  	type = select["answer"];//rre-evaluate
-  	showAnswer = select["showAnswer"];
-  	$("#name").html(select["name"]);
-  	$("#generate").hide();
-  	$("#result").hide();
-  
+// If/else for correct and incorrect
+	if (question1 == "pokémon") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-  });
+	if (question2 == "pokémon") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question3 == "both") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question4 == "pokémon") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question5 == "both") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question6 == "word") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question7 == "word") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question8 == "word") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question9 == "pokémon") {
+		correct++;
+	} else {
+		incorrect++
+	}
+
+	if (question10 == "word") {
+		correct++;
+	} else {
+		incorrect++
+	}
+catchy.play();
+document.getElementById("after_submit").style.visibility ="visible";
+document.getElementById("number_correct").innerHTML = "You got " + correct + " correct!";
+document.getElementById("number_incorrect").innerHTML = "You got " + incorrect + " incorrect.";
+}
+
+//empty function reloads page
+function reset(){}
